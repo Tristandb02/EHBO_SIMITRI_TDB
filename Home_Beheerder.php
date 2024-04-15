09c8f61f8346975efbd7d8d7b1ceb0387afe9114
tristand101006 <debentristan@gmail.com>
2024-04-15 11:27:44 +0200

    login

--- Home_Beheerder.php
+++ Home_Beheerder.php
@@ -2,12 +2,17 @@
 <form method="post">
 <h1>Welkom beheerder</h1>
 <input type="submit" value="Ga naar klassen overzicht" name="btnGaKlassen">
+<input type="submit" value="Maak account aan" name="btnAccount">
 </form>
 </html>
 <?php
 if(isset($_POST["btnGaKlassen"]))
 {
     header("location: overzichtKlas.php");
+}
+if(isset($_POST["btnAccount"]))
+{
+    header("location: Gebruiker_toevoegen.php");
 }
 
 ?>
---
+++ Wachtwoord_Aanpassen.php
@@ -0,0 +1,2 @@
+<?php
+echo "ewa drerie";
