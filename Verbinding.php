0888c598ebaec1070bb4257102fd528de22fb825
tristand101006 <debentristan@gmail.com>
2024-05-13 11:18:44 +0200

    Bestaande categorie toevoegen

--- Verbinding.php
+++ Verbinding.php
@@ -4,6 +4,6 @@ Author: Tristan De Ben
 <?php
 
 //verbinding naar de database
-$link = mysqli_connect('localhost', 'root', '','db_ehbo');
+$link = mysqli_connect('gtiictbeokcommon.mysql.db', 'gtiictbeokcommon', 'Beveren9120','gtiictbeokcommon');
 
 ?>
