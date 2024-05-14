0de9b1e6a6b8bde785cb97bd486dce8443f0b4be
tristand101006 <debentristan@gmail.com>
2024-05-14 09:09:29 +0200

    mail in template met wat aanpassingen

--- Template/hospital-website-template/testimonial.php
+++ Template/hospital-website-template/testimonial.php
@@ -165,20 +165,105 @@
                     <h4 class="d-inline-block text-primary text-uppercase border-bottom border-5 border-secondary mb-4">Get In Touch</h4>
                     <?php
                     //verbinding met database
-                    include('Verbinding.php');
+                    include 'Verbinding.php';
+                    $query = "SELECT COLUMN_NAME 
+          FROM INFORMATION_SCHEMA.COLUMNS 
+          WHERE TABLE_SCHEMA = 'db_ehbo' 
+          AND TABLE_NAME = 'dozen'
+          LIMIT 2, 999"; // Skipping the first two columns
 
-                    // mail sturen
-                    //$to = $_POST['mail'];
-                    //echo 'mail';
-                    $testName = 'tristand101006@student.gtibeveren.be';
-                    $to = $testName; //extra ontvanger toevoegen $_POST['mail'].','.
-                    $from = 'tristand101006@student.gtibeveren.be';
-                    $fromName = 'GTI - EHBO';
+                    $stmt = mysqli_stmt_init($link);
+                    $columnNames = array(); // Initialize an array to store column names
 
-                    $subject = "Inschrijving kaas- en wijnavond GTI";
+                    if (mysqli_stmt_prepare($stmt, $query)) {
+                        mysqli_stmt_execute($stmt);
+                        $res = mysqli_stmt_get_result($stmt);
 
-                    $htmlContent =
-                        '<html> 
+                        echo "<table border='1'>";
+                        echo "<tr>";
+                        echo "<html>
+<form method='post'>
+    <select name='Item' >";
+                        while ($row = mysqli_fetch_assoc($res)) {
+
+                            echo "<option value='" . $row['COLUMN_NAME'] . "'>" . $row['COLUMN_NAME'] . "</option>";
+                        }
+                    }
+                    echo "<input type='submit' name='btnZoek' value='Laat zien'><br><input type='submit' name='btnTerug' value='Ga terug'><br>";
+                    echo "<input type='submit' name='btnSend' value='Verstuur op mail'>";
+
+
+
+                    $LokalenOntbreek="";
+                    $intLokalenOntbreek=0;
+
+                    if(isset($_POST["Item"]))
+                    {
+                        if($_POST["Item"]!="handschoenen")
+                        {
+                            if(mysqli_stmt_prepare($stmt,"select lokaal from db_ehbo.dozen where ".$_POST["Item"]." = 'Niet Aanwezig'"))
+                            {
+                                mysqli_stmt_execute($stmt);
+                                $res = mysqli_stmt_get_result($stmt);
+                                while ($row=mysqli_fetch_assoc($res))
+                                {
+                                    $intLokalenOntbreek++;
+                                    $LokalenOntbreek.=$row["lokaal"].", ";
+
+                                }
+
+                                $LokalenOntbreek = substr($LokalenOntbreek, 0, -2);
+                                if ($intLokalenOntbreek != 0)
+                                {
+                                    $Ontbreek = "Er ontbreken ".$intLokalenOntbreek." ".$_POST["Item"]."  in de volgende lokalen: ".$LokalenOntbreek;
+                                    $_SESSION['Ontbrekend'] = $Ontbreek;
+                                    echo "$Ontbreek";
+                                }
+                                else
+                                {
+                                    echo "Er ontbreken geen ".$_POST["Item"];
+                                }
+
+                            }
+
+                        }
+                        else {
+                            if (mysqli_stmt_prepare($stmt, "select lokaal from db_ehbo.dozen where " . $_POST["Item"] . " = 1")) {
+                                mysqli_stmt_execute($stmt);
+                                $res = mysqli_stmt_get_result($stmt);
+                                while ($row = mysqli_fetch_assoc($res)) {
+                                    $intLokalenOntbreek++;
+                                    $LokalenOntbreek .= $row["lokaal"] . ", ";
+
+                                }
+
+                                $LokalenOntbreek = substr($LokalenOntbreek, 0, -2);
+                                if ($intLokalenOntbreek != 0) {
+                                    $Ontbreek = "Er zijn  " . $intLokalenOntbreek . " lokalen waar er maar 1 paar " . $_POST["Item"] . "  ligt, en dat is in de volgende lokalen: " . $LokalenOntbreek;
+                                    $_SESSION['Ontbrekend'] = $Ontbreek;
+                                    echo "$Ontbreek";
+                                } else {
+                                    echo "Er ontbreken geen " . $_POST["Item"];
+                                }
+
+                            }
+                        }
+
+                    }
+                    if(isset($_POST['btnSend']))
+                    {
+                        // mail sturen
+                        //$to = $_POST['mail'];
+                        //echo 'mail';
+                        $testName = 'tristand101006@student.gtibeveren.be';
+                        $to = $testName; //extra ontvanger toevoegen $_POST['mail'].','.
+                        $from = 'tristand101006@student.gtibeveren.be';
+                        $fromName = 'GTI - EHBO';
+
+                        $subject = "Inschrijving kaas- en wijnavond GTI";
+
+                        $htmlContent =
+                            '<html> 
     <head> 
         <title>Ontbreken EHBO doosjes</title> 
     </head> 
@@ -188,20 +273,25 @@
     </body> 
     </html>';
 
-                    // Set content-type header for sending HTML email
-                    $headers = "MIME-Version: 1.0" . "\r\n";
-                    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+                        // Set content-type header for sending HTML email
+                        $headers = "MIME-Version: 1.0" . "\r\n";
+                        $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 
-                    // Additional headers
-                    $headers .= 'From: '.$fromName.'<'.$from.'>' . "\r\n";
-                    $testName = 'tristand101006@student.gtibeveren.be'; //werkt om mail in cc te krijgen
-                    $headers .= 'Cc:'.$testName. "\r\n";
+                        // Additional headers
+                        $headers .= 'From: '.$fromName.'<'.$from.'>' . "\r\n";
+                        $testName = 'tristand101006@student.gtibeveren.be'; //werkt om mail in cc te krijgen
+                        $headers .= 'Cc:'.$testName. "\r\n";
 
-                    // Send email
-                    if(mail($to, $subject, $htmlContent, $headers)){
-                        echo 'ok';
-                    }else{
-                        echo 'Email sending failed.';
+                        // Send email
+                        if(mail($to, $subject, $htmlContent, $headers)){
+                            echo 'ok';
+                        }else{
+                            echo 'Email sending failed.';
+                        }
+                    }
+                    if(isset($_POST["btnTerug"]))
+                    {
+                        header("Location: Home_Beheerder.php");
                     }
 
                     ?>
