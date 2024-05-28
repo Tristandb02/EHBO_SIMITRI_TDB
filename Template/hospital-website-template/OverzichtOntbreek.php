ed800f7366463ae91d468a4ecefb64d1c39a3266
tristand101006 <debentristan@gmail.com>
2024-05-28 09:33:40 +0200

    mail en commentaar OverzichtOntbreek.php

--- Template/hospital-website-template/OverzichtOntbreek.php
+++ Template/hospital-website-template/OverzichtOntbreek.php
@@ -175,9 +175,9 @@ $intLokalenOntbreek=0;
 
 if(isset($_POST["Item"]))
 {
-    if($_POST["Item"]!="handschoenen") //Handschoenen zin met waardes niet met aan of afweizg
+    if($_POST["Item"]!="handschoenen") //Handschoenen zijn met waardes niet met aan of afwezig
     {
-        if(mysqli_stmt_prepare($stmt,"select lokaal from EHBO_dozen where ".$_POST["Item"]." = 'Niet Aanwezig'"))
+        if(mysqli_stmt_prepare($stmt,"select lokaal from EHBO_dozen where ".$_POST["Item"]." = 'Niet Aanwezig'")) //De lokalen van het item ophalen waar het niet aanwezig is
         {
             mysqli_stmt_execute($stmt);
             $res = mysqli_stmt_get_result($stmt);
@@ -196,7 +196,7 @@ if(isset($_POST["Item"]))
                 echo "$Ontbreek";
 
             }
-            else
+            else //Als er geen items ontbreken schrijven we dit naar het scherm
             {
                 echo "Er ontbreken geen ".$_POST["Item"];
 
@@ -315,8 +315,8 @@ if(isset($_POST["btnTerug"]))
         }
 
 
-        $to = 'tristand101006@student.gtibeveren.be'; // Extra ontvanger toevoegen via $_POST['mail'] indien nodig
-        $from = 'tristand101006@student.gtibeveren.be';
+        $to = $_SESSION['Mail']; // Extra ontvanger toevoegen via $_POST['mail'] indien nodig
+        $from = 'EHBO.gtibeveren.be';
         $fromName = 'GTI - EHBO';
 
         $subject = "Ontbrekende in EHBO doosjes";
@@ -337,7 +337,7 @@ if(isset($_POST["btnTerug"]))
 
         // Additional headers
         $headers .= 'From: ' . $fromName . '<' . $from . '>' . "\r\n";
-        $headers .= 'Cc: tristand101006@student.gtibeveren.be' . "\r\n";
+        $headers .= 'Cc: '. $_SESSION["MAIL"] . "\r\n";
 
         // Send email
         if (mail($to, $subject, $htmlContent, $headers)) {
@@ -361,69 +361,7 @@ if(isset($_POST["btnTerug"]))
 
 
     <!-- Footer Start -->
-    <div class="container-fluid bg-dark text-light mt-5 py-5">
-        <div class="container py-5">
-            <div class="row g-5">
-                <div class="col-lg-3 col-md-6">
-                    <h4 class="d-inline-block text-primary text-uppercase border-bottom border-5 border-secondary mb-4">Get In Touch</h4>
 
-                    <p class="mb-2"><i class="fa fa-map-marker-alt text-primary me-3"></i>123 Street, New York, USA</p>
-                    <p class="mb-2"><i class="fa fa-envelope text-primary me-3"></i>info@example.com</p>
-                    <p class="mb-0"><i class="fa fa-phone-alt text-primary me-3"></i>+012 345 67890</p>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <h4 class="d-inline-block text-primary text-uppercase border-bottom border-5 border-secondary mb-4">Quick Links</h4>
-                    <div class="d-flex flex-column justify-content-start">
-                        <a class="text-light mb-2" href="#"><i class="fa fa-angle-right me-2"></i>Home</a>
-                        <a class="text-light mb-2" href="#"><i class="fa fa-angle-right me-2"></i>About Us</a>
-                        <a class="text-light mb-2" href="#"><i class="fa fa-angle-right me-2"></i>Our Services</a>
-                        <a class="text-light mb-2" href="#"><i class="fa fa-angle-right me-2"></i>Meet The Team</a>
-                        <a class="text-light mb-2" href="#"><i class="fa fa-angle-right me-2"></i>Latest Blog</a>
-                        <a class="text-light" href="#"><i class="fa fa-angle-right me-2"></i>Contact Us</a>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <h4 class="d-inline-block text-primary text-uppercase border-bottom border-5 border-secondary mb-4">Popular Links</h4>
-                    <div class="d-flex flex-column justify-content-start">
-                        <a class="text-light mb-2" href="#"><i class="fa fa-angle-right me-2"></i>Home</a>
-                        <a class="text-light mb-2" href="#"><i class="fa fa-angle-right me-2"></i>About Us</a>
-                        <a class="text-light mb-2" href="#"><i class="fa fa-angle-right me-2"></i>Our Services</a>
-                        <a class="text-light mb-2" href="#"><i class="fa fa-angle-right me-2"></i>Meet The Team</a>
-                        <a class="text-light mb-2" href="#"><i class="fa fa-angle-right me-2"></i>Latest Blog</a>
-                        <a class="text-light" href="#"><i class="fa fa-angle-right me-2"></i>Contact Us</a>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <h4 class="d-inline-block text-primary text-uppercase border-bottom border-5 border-secondary mb-4">Newsletter</h4>
-                    <form action="">
-                        <div class="input-group">
-                            <input type="text" class="form-control p-3 border-0" placeholder="Your Email Address">
-                            <button class="btn btn-primary">Sign Up</button>
-                        </div>
-                    </form>
-                    <h6 class="text-primary text-uppercase mt-4 mb-3">Follow Us</h6>
-                    <div class="d-flex">
-                        <a class="btn btn-lg btn-primary btn-lg-square rounded-circle me-2" href="#"><i class="fab fa-twitter"></i></a>
-                        <a class="btn btn-lg btn-primary btn-lg-square rounded-circle me-2" href="#"><i class="fab fa-facebook-f"></i></a>
-                        <a class="btn btn-lg btn-primary btn-lg-square rounded-circle me-2" href="#"><i class="fab fa-linkedin-in"></i></a>
-                        <a class="btn btn-lg btn-primary btn-lg-square rounded-circle" href="#"><i class="fab fa-instagram"></i></a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="container-fluid bg-dark text-light border-top border-secondary py-4">
-        <div class="container">
-            <div class="row g-5">
-                <div class="col-md-6 text-center text-md-start">
-                    <p class="mb-md-0">&copy; <a class="text-primary" href="#">Your Site Name</a>. All Rights Reserved.</p>
-                </div>
-                <div class="col-md-6 text-center text-md-end">
-                    <p class="mb-0">Designed by <a class="text-primary" href="https://htmlcodex.com">HTML Codex</a></p>
-                </div>
-            </div>
-        </div>
-    </div>
     <!-- Footer End -->
 
 
