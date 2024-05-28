633437717372c99b70f2e17e63b4e9ec112b286a
simonm020305 <simon.marchand3@gmail.com>
2024-05-28 09:40:31 +0200

    overal de takenbalk aan gepast

--- Template/hospital-website-template/OverzichtKlas.php
+++ Template/hospital-website-template/OverzichtKlas.php
@@ -48,8 +48,8 @@ if (isset($_POST["klaslink"])) { // Controleer of een knop is ingedrukt
 <div class="container-fluid sticky-top bg-white shadow-sm">
     <div class="container">
         <nav class="navbar navbar-expand-lg bg-white navbar-light py-3 py-lg-0">
-            <a href="index.html" class="navbar-brand">
-                <h1 class="m-0 text-uppercase text-primary"><i class="fa fa-clinic-medical me-2"></i>Medinova</h1>
+            <a href="https://www.beveren.be/nl/scholen/gti-beveren"  class="navbar-brand">
+                <h1 class="m-0 text-uppercase text-primary">EHBO</h1>
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
                 <span class="navbar-toggler-icon"></span>
