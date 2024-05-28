05c9ea53b980755128dd318213c979beacc919a3
milanv171104 <milanvanwonterghem@gmail.com>
2024-05-28 09:23:01 +0200

    kleine aanpassing home beheerder

--- Template/hospital-website-template/Home_Beheerder.php
+++ Template/hospital-website-template/Home_Beheerder.php
@@ -78,7 +78,7 @@
             <div class="row justify-content-start">
                 <div class="col-lg-8 text-center text-lg-start">
                     <h5 class="d-inline-block text-primary text-uppercase border-bottom border-5" style="border-color: rgba(256, 256, 256, .3) !important;">EHBO</h5>
-                    <h1 class="display-1 text-white mb-md-4">Welcome Beheerder</h1>
+                    <h1 class="display-1 text-white mb-md-4">Welkom Beheerder</h1>
                     <div class="pt-2">
                         <a href="OverzichtKlas.php" class="btn btn-light rounded-pill py-md-3 px-md-5 mx-2">Klassen overzicht</a>
                         <a href="index.php" class="btn btn-outline-light rounded-pill py-md-3 px-md-5 mx-2">Afmelden</a>
